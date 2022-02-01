ed1ad94b5b1092469ed31499b3569338909d18ce
serkan atesyak <serkanatesyak@hotmail.com>
2022-02-02 01:11:02 +0300

    Add project files.

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntitiyFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+    public class BlogController : Controller
+    {
+
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        public IActionResult Index()
+        {
+
+            var values = bm.GetBlogListWithCategory();
+            return View(values);
+        }
+
+        public IActionResult BlogReadAll(int id)
+        {
+
+            var values = bm.GetBlogByID(id);
+            return View(values);
+
+
+        }
+
+
+
+    }
+}
---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntitiyFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        CategoryManager cm = new CategoryManager(new EfCategoryRepository());   //cm nesnesi ile bütün methotlara erişim sağlıcam category managerdeki metholara ulaşım sağlıuyoruz
+        public IActionResult Index()
+        {
+            var values = cm.GetList();
+            return View(values);
+        }
+
+       
+
+
+    }
+}
