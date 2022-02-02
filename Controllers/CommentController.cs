a60ed54b38273b6c953fca26e48a1c21e44a031b
serkan atesyak <serkanatesyak@hotmail.com>
2022-02-03 00:01:06 +0300

    view component ve yorumların listelenmesi

--- Controllers/CommentController.cs
+++ Controllers/CommentController.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntitiyFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
     public class CommentController : Controller
     {
+
+        CommentManager cm = new CommentManager(new EfCommentRepository());
         public IActionResult Index()
         {
             return View();
@@ -14,9 +18,10 @@ namespace CoreDemo.Controllers
         {
             return PartialView();
         }
-        public PartialViewResult CommentListByBlog()
+        public PartialViewResult CommentListByBlog(int id)
         {
-            return PartialView();
+            var values = cm.GetList(id);
+            return PartialView(values);
         }
     }
 }
---
+++ ViewComponents/CommentList.cs
@@ -0,0 +1,33 @@
+﻿using CoreDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CoreDemo.ViewComponents
+{
+    public class CommentList : ViewComponent
+    {
+
+
+        public IViewComponentResult Invoke()
+        {
+            var commentvalues = new List<UserComment>
+            {
+                new UserComment
+                {
+                    ID = 1,UserName="Serkan"
+                },
+                 new UserComment
+                {
+                    ID = 2,UserName="Erkan"
+                },
+                  new UserComment
+                {
+                    ID = 3,UserName="Can"
+                }
+
+            };
+            return View(commentvalues);
+        }
+
+    }
+}
